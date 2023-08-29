0f0c1672b9445c648960bb6da1ebfa1a00784cd5
Abdullah <73560425+zer-far@users.noreply.github.com>
2023-08-29 15:14:39 +0100

    Improve oddecho.py

--- oddecho.py
+++ oddecho.py
@@ -1,8 +1,7 @@
 lst = []
 N = int(input())
 
-for i in range(0, N):
-    ele = str(input())
-    lst.append(ele)
+for _ in range(0, N):
+    lst.append(str(input()))
 for i in range(0, N, 2):
     print(lst[i])
